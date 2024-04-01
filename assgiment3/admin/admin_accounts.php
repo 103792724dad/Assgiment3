0a700dc93e119cb05fedf98cda879d1b4f0d259a
103792724dad <Nthuhien6789@gmail.com>
2024-04-01 11:23:07 +0700

    SetUp

---
+++ assgiment3/admin/admin_accounts.php
@@ -0,0 +1,20 @@
+<?php
+
+include '../components/connect.php';
+
+session_start();
+
+$admin_id = $_SESSION['admin_id'];
+
+if(!isset($admin_id)){
+   header('location:admin_login.php');
+}
+
+if(isset($_GET['delete'])){
+   $delete_id = $_GET['delete'];
+   $delete_admin = $conn->prepare("DELETE FROM `admin` WHERE id = ?");
+   $delete_admin->execute([$delete_id]);
+   header('location:admin_accounts.php');
+}
+
+?>
